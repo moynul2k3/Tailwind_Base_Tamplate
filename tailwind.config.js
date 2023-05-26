196aa2821d20223b5a5f1b5378d721e333b28794
Moynul Islam <moynul.officials@gmail.com>
2023-05-26 20:48:49 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+    color: {
+      "brandClr": "#212e50"
+
+    },
+  },
+  plugins: [],
+}
+
